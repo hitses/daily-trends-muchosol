1b1afbeb7ca73448d78c8a93f8e70402bd86ea4a
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-05-05 07:55:47 +0200

    A - Newspaper constants

---
+++ src/constants/newspapersElements.ts
@@ -0,0 +1,15 @@
+export const PAIS = Object.freeze({
+  URL: 'https://elpais.com/'
+})
+
+export const MUNDO = Object.freeze({
+  URL: 'https://www.elmundo.es/',
+  ANCHOR_CLASS: 'a.ue-c-cover-content__link',
+  H2_CLASS: 'h2.ue-c-cover-content__headline'
+})
+
+export const USER_AGENT = Object.freeze({
+  userAgent:
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' +
+    ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+})
