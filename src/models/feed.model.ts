04532d685bbeaf7c5cc6f78bba3be633e940a452
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-05-06 12:54:02 +0200

    U - Limit the "newspaper" options to "El País" and "El Mundo"

--- src/models/feed.model.ts
+++ src/models/feed.model.ts
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose'
 
 const feedSchema = new Schema(
   {
-    newspaper: { type: String },
+    newspaper: { type: String, enum: ['El País', 'El Mundo'], required: true },
     text: { type: String, required: true, unique: true },
     href: { type: String, required: true, unique: true },
     delete: { type: Boolean, required: true, default: false }
