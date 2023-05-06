ca7cf32936c804867883a8cf1814b3923024aad2
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-05-06 12:54:46 +0200

    A - Get the first 5 elements of each type of newspaper (newspaper) according to the update date

--- src/services/feed/getFeeds.service.ts
+++ src/services/feed/getFeeds.service.ts
@@ -1,8 +1,45 @@
-// import Feed from 'feeds model route'
+import feedModel from '../../models/feed.model'
 
 export const getFeedsService = async () => {
   try {
-    return { status: 200, data: { message: 'Get Feeds Service' } }
+    const result = await feedModel.aggregate([
+      // Get the five front page stories of each newspaper
+      { $sort: { updatedAt: -1 } },
+      {
+        $group: {
+          _id: '$newspaper',
+          feeds: { $push: '$$ROOT' }
+        }
+      },
+      {
+        $project: {
+          newspaper: '$_id',
+          feeds: {
+            $slice: [
+              {
+                $slice: ['$feeds', 5]
+              },
+              0,
+              5
+            ]
+          }
+        }
+      },
+      { $unwind: '$feeds' },
+      {
+        $project: {
+          _id: '$feeds._id',
+          text: '$feeds.text',
+          href: '$feeds.href',
+          newspaper: '$newspaper',
+          updatedAt: '$feeds.updatedAt'
+        }
+      },
+      { $sort: { updatedAt: -1 } },
+      { $limit: 10 }
+    ])
+
+    return { status: 200, data: result }
   } catch (err) {
     return { status: 500, data: { message: 'Get Feeds Service', err } }
   }
