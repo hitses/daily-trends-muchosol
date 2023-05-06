222895b59ab7a2aa84b06e67e7c4ed29a3dc3593
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-05-06 12:30:10 +0200

    BUG - Properly written code in its corresponding files // Usage test of all Git commit messages

--- src/controllers/feed/createFeed.controller.ts
+++ src/controllers/feed/createFeed.controller.ts
@@ -1,26 +1,7 @@
-import { FeedInterface } from '../../models/feed.interface'
-import feedModel from '../../models/feed.model'
+import { Request, Response } from 'express'
+import { createFeedsService } from '../../services'
 
-export const createFeeds = async (feeds: FeedInterface[]) => {
-  try {
-    // Get feeds that already exist in the database
-    const existingFeeds: FeedInterface[] = await feedModel
-      .find(
-        { href: { $in: feeds.map(feed => feed.href) } },
-        { _id: 0, href: 1 }
-      )
-      .lean()
-
-    // Filter feeds to get only those that do not exist in the database
-    const newFeeds: FeedInterface[] = feeds.filter(
-      feed =>
-        !existingFeeds.some(existingFeed => existingFeed.href === feed.href)
-    )
-
-    // Insert new feeds into the database
-    if (newFeeds.length > 0)
-      await feedModel.insertMany(newFeeds, { ordered: false })
-  } catch (err) {
-    console.log(err)
-  }
+export const createFeeds = async (_req: Request, res: Response) => {
+  const { status, data } = await createFeedsService()
+  return res.status(status).send(data)
 }
--- src/services/feed/createFeed.service.ts
+++ src/services/feed/createFeed.service.ts
@@ -1,27 +1,49 @@
-import { createFeeds } from '../../controllers'
 import { scrapElMundo, scrapElPais } from '../../helpers/playwright'
 import { FeedInterface } from '../../models/feed.interface'
+import feedModel from '../../models/feed.model'
 
-export const createFeedService = async () => {
+export const createFeedsService = async () => {
   try {
+    // Get by scraping feeds from El Mundo and El País
     const elMundo = (await scrapElMundo()) as FeedInterface[]
     const elPais = (await scrapElPais()) as FeedInterface[]
 
     let mundoTimeout = false
     let paisTimeout = false
 
+    // Check if scraping failed and return timeout by El Mundo
     if (typeof elMundo === 'string' && elMundo === 'Timeout')
       mundoTimeout = true
-    else await createFeeds(elMundo)
 
+    // Check if scraping failed and return timeout by El País
     if (typeof elPais === 'string' && elPais === 'Timeout') paisTimeout = true
-    else await createFeeds(elPais)
 
+    // Return timeout if scraping failed
     if (mundoTimeout || paisTimeout)
       return {
         status: 408,
         data: { mundoTimeout, paisTimeout, message: 'Timeout. Try again' }
       }
+
+    const feeds = [...elMundo, ...elPais]
+
+    // Get feeds that already exist in the database
+    const existingFeeds: FeedInterface[] = await feedModel
+      .find(
+        { href: { $in: feeds.map(feed => feed.href) } },
+        { _id: 0, href: 1 }
+      )
+      .lean()
+
+    // Filter feeds to get only those that do not exist in the database
+    const newFeeds: FeedInterface[] = feeds.filter(
+      feed =>
+        !existingFeeds.some(existingFeed => existingFeed.href === feed.href)
+    )
+
+    // Insert new feeds into the database
+    if (newFeeds.length > 0)
+      await feedModel.insertMany(newFeeds, { ordered: false })
 
     return { status: 201, data: { message: 'Feeds created' } }
   } catch (err) {
